2b9c37cb25419f27dbef0405c4619b6d113c2d83
DishaGup <guptamansi201@gmail.com>
2022-10-26 08:40:44 +0530

    Good

---
+++ substring.js
@@ -0,0 +1,14 @@
+**Finding the substring sequence of given string**
+
+function Substring(string){
+
+for(let i=0;i<string.length;i++){
+
+for(let j=0;j<string.length;j++){
+
+return string[j];
+}}}
+
+
+let ans=Substring("Superman")
+console.log("ans")
